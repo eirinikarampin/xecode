ed56ec31d045560176507e4de08fbab57b847c83
ekarampina <eirini.karampina@arhs-dev-hellas.com>
2025-03-11 13:53:56 +0200

    update headless mode

--- src/main/java/org/scenarios/tc01.java
+++ src/main/java/org/scenarios/tc01.java
@@ -22,7 +22,7 @@ public class tc01 {
     void launchBrowser() {
         playwright = Playwright.create();
         browser = playwright.chromium().launch(new BrowserType.LaunchOptions()
-                .setHeadless(false)
+                .setHeadless(true)
                 .setSlowMo(500));
     }
 
